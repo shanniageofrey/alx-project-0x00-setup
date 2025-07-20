c7cf4ebc4bc1d456de4a65fea33cc62ba6ed848f
Shannia <shanniageofrey@gmail.com>
2025-07-20 11:44:10 +0300

    Basic Routing

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/qsbWBi2uy8o1LswuOWEeD56dlS2/WtoiUTk3NoqVaI=\"\n}"
--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Welcome to ALX Project 🚀</h1>
-      <p>This is the homepage.</p>
-    </div>
-  );
-}
