6da214b0321f08c0fd2021da3a174713c642a9ed
Shannia <shanniageofrey@gmail.com>
2025-07-20 13:42:16 +0300

    Button Props update

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -11,9 +11,9 @@ return (
     <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-lg ">
       <Image className="rounded-lg" src={HOUSE_IMAGE} width={378.56} height={299.37} alt="house image" />
       <div className="p-2 flex gap-2 mt-2">
-        <Pill title="Top Villa" />
-        <Pill title="Self CheckIn" />
-        <Pill title="Free Reschedule" />
+        <Pill title="Top Villa" styles="" />
+        <Pill title="Self CheckIn"  styles=""/>
+        <Pill title="Free Reschedule"  styles=""/>
       </div>
       <div className="flex items-center justify-between">
         <div>
--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,4 +1,4 @@
 export interface ButtonProps {
   title: string;
-  styles?:string; //Optional string for custom Tailwind classes (e.g., size, shape, color)
+  styles:string; //Optional string for custom Tailwind classes (e.g., size, shape, color)
 } //we define title is a string type, so that we can pass it as a prop to the Pill & Button component.
