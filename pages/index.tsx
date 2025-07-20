216bfdc10c2063df26eb4d2af2a9d8a3fb76dd0f
Shannia <shanniageofrey@gmail.com>
2025-07-20 10:40:10 +0300

    update index.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
-
-const Home: React.FC = () => {
+export default function Home() {
   return (
-  <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
-      <h1>Airbnb Application Clone system</h1>
-      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
-    </main>
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to ALX Project 🚀</h1>
+      <p>This is the homepage.</p>
+    </div>
   );
 }
-
-export default Home;
